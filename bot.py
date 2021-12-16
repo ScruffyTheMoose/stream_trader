11a05861bfb6b32f74eecdb604450cd321b64e84
Jamie <ScruffyTheMoose@users.noreply.github.com>
2021-12-15 22:57:04 -0600

    add uptime tracker

--- bot.py
+++ bot.py
@@ -1,65 +1,83 @@
 import pytchat
 from broker import PaperTrade # to initiate paper trading instance
+import time
+import datetime
 
 # example chat access for reference
 # print(f"{c.datetime} [{c.author.name}]- {c.message}")
 
-def run():
+class Bot:
 
-    # get input for initial cash stack and initiate paper trading instance
-    # start_cash = input("Enter starting cash stack:")
-    init_balance = input("Enter initial trade balance: ")
-    pt = PaperTrade(init_balance)
+    def __init__(self) -> None:
+        """Constructor"""
+        self.start_time = time.time()
 
-    # get input for channel ID and initiate chat instance
-    # channel = input("Enter stream ID:")
-    stream_id = input("Enter Youtube stream ID: ")
-    chat = pytchat.create(video_id=stream_id)
+    def getUptime(self) -> None:
+        uptime_sec = time.time() - self.start_time
+        conversion = datetime.timedelta(seconds=uptime_sec)
+        uptime_min = str(conversion)
 
-    # print to log that bot is running
-    print(f"""
-                == Bot is running ==
-            Stream ID: {stream_id}
-            Initial Balance: {pt.bank}
+        print(f"""
+            Current uptime: {uptime_min} minutes
         """)
 
-    # continuously evaluating new chat messages
-    while ( chat.is_alive() ):
+    def run():
+        """Initiates chat monitor and trading"""
 
-        # for each element in the continuously updated log
-        for c in chat.get().sync_items():
-            
-            # checking if the message contains command prefix
-            if c.message[0] == '!':
+        # get input for initial cash stack and initiate paper trading instance
+        # start_cash = input("Enter starting cash stack:")
+        init_balance = input("Enter initial trade balance: ")
+        pt = PaperTrade(init_balance)
 
-                order = c.message.split(' ')
+        # get input for channel ID and initiate chat instance
+        # channel = input("Enter stream ID:")
+        stream_id = input("Enter Youtube stream ID: ")
+        chat = pytchat.create(video_id=stream_id)
+
+        # print to log that bot is running
+        print(f"""
+                    == Bot is running ==
+                Stream ID: {stream_id}
+                Initial Balance: {pt.bank}
+            """)
+
+        # continuously evaluating new chat messages
+        while ( chat.is_alive() ):
+
+            # for each element in the continuously updated log
+            for c in chat.get().sync_items():
                 
-                try:
-                    command = order[0]
-                    ticker = order[1]
-                except:
-                    command = order[0]
-                    ticker = 'N/A'
-                
-                # if buy command
-                if command == '!buy':
-                    pt.buy(ticker)
+                # checking if the message contains command prefix
+                if c.message[0] == '!':
 
-                # if sell command
-                elif command == '!sell':
-                    pt.sell(ticker)
+                    order = c.message.split(' ')
+                    
+                    try:
+                        command = order[0]
+                        ticker = order[1]
+                    except:
+                        command = order[0]
+                        ticker = 'N/A'
+                    
+                    # if buy command
+                    if command == '!buy':
+                        pt.buy(ticker)
 
-                elif command == '!update':
-                    pt.getUpdate()
+                    # if sell command
+                    elif command == '!sell':
+                        pt.sell(ticker)
 
-                # no matching command, produce command error
-                else:
-                    pt.commandError()
+                    elif command == '!update':
+                        pt.getUpdate()
 
-            if pt.order_count > 0 and pt.order_count % 10 == 0:
-                pt.getValue()
-                pt.getpnl()
+                    # no matching command, produce command error
+                    else:
+                        pt.commandError()
+
+                if pt.order_count > 0 and pt.order_count % 10 == 0:
+                    pt.getValue()
+                    pt.getpnl()
 
 
 if __name__ == "__main__":
-    run()
+    Bot().run()
